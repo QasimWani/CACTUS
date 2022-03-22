687d51fb529c3e71ef3ab1843af1d678cd3a889f
Qasim Wani <qasim31wani@gmail.com>
2022-03-21 21:35:53 -0400

    replacing timestamp with number

--- models/bluetooth.js
+++ models/bluetooth.js
@@ -22,7 +22,7 @@ var SniffSchema = new mongoose.Schema({
                 UUID : String
             }
         ],
-        _timestamp : String, // time of capture
+        _timestamp : Number, // time of capture
     }
 });
 
