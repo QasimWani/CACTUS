0f82b6d53af45480bd3c8710514cec54dd2f638f
Qasim Wani <43754306+QasimWani@users.noreply.github.com>
2022-03-27 16:35:41 -0400

    Update capture.sh

--- wifi_sniffing/capture.sh
+++ wifi_sniffing/capture.sh
@@ -3,6 +3,7 @@ x=1
 while [ $x -gt 0 ]
 do
 	sudo tcpdump -i wlan1 -c 240 -w capture$x.pcap
-	tshark -V -r capture$x.pcap > ~/wificaptures/capture$x.txt
+	tshark -V -r capture$x.pcap > ~/CACTUS/DB/capture/wifi/capture$x.txt
+	rm -f capture$x.pcap
 	x=$(( $x+1))
 done
