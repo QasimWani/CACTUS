ddf7d9ac671c0488c0f98e90beb8a3fbc43ca66e
joser12345678 <joser17@vt.edu>
2022-04-10 23:09:24 +0100

    added sample for qasim to parse AND changes to capture.sh, rotates between 2.4 and 5 ghz now

--- wifi_sniffing/capture.sh
+++ wifi_sniffing/capture.sh
@@ -2,6 +2,21 @@
 x=1
 while [ $x -gt 0 ]
 do
+	if [ $(($x%2)) -eq 0 ];
+	then
+		echo "changing to 5ghz"
+		sudo ip link set wlan1 down
+		sudo iw dev wlan1 set type monitor
+		sudo iwconfig wlan1 channel 36
+		sudo ip link set wlan1 up
+	else
+		echo "changing to 2.4ghz"
+		sudo ip link set wlan1 down
+		sudo iw dev wlan1 set type monitor
+		sudo iwconfig wlan1 channel 1
+		sudo ip link set wlan1 up
+	fi
+
 	sudo tcpdump -i wlan1 -c 240 -w capture$x.pcap
 	tshark -V -r capture$x.pcap > ~/CACTUS/DB/capture/wifi/capture$x.txt
 	rm -f capture$x.pcap
